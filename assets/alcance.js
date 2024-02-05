92c593da7830df0fa63e2e7160a70a997a1f54fd
alejosebastianali <74724530+alejosebastianali@users.noreply.github.com>
2024-02-05 09:24:27 -0300

    Add files via upload

---
+++ assets/alcance.js
@@ -0,0 +1,16 @@
+$('.progress-number').each(function() {
+    $(this).animate({
+      left: $(this).data('number')
+    }, {
+      duration: 10000,
+      step: function(now) {
+        var data = Math.round(now);
+        $(this).text('+' + data );
+      }
+    });
+  });
+  
+  //Em An
+  // 14-12-2016
+  // Update: https://codepen.io/eslam-zaki/pen/egavEz
+  // https://codepen.io/Em-An/pen/PJZWLz
